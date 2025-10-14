bd405c8868fe0745e531c06dc4330d76e84f1ec3
Dmitrii Sharshakov <d3dx12.xx@gmail.com>
2025-10-14 16:16:48 +0200

    WIP (should be in env) DFU kernel development config

--- include/configs/rv1106_common.h
+++ include/configs/rv1106_common.h
@@ -45,7 +45,8 @@
 	"bootcmd=mw.l 0xff020200 0xEF08A53C;"                                                                                                  \
 	"setenv dfu_alt_info \"ram 0=uImage ram 0x2000000 0x1400000;devicetree.dtb ram 0x3400000 0x400000;initcpio ram 0x5d00000 0x2000000\";" \
 	"dfu 0;"                                                                                                                               \
-	"setenv bootargs 'earlycon=uart8250,mmio32,0xff4c0000 console=ttyS2 cma=1M ignore_loglevel clk_ignore_unused oops=panic panic=1';\0"   \
+	"setenv bootargs 'earlycon=uart8250,mmio32,0xff4c0000 console=ttyS2,115200 cma=1M ignore_loglevel tp_printk oops=panic panic=1';" \
+	"bootz 0x02000000 0x5d00000:0x6a3400 0x03400000;\0"   \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"partitions=" PARTS_DEFAULT \
 	ENV_MEM_LAYOUT_SETTINGS	 \
